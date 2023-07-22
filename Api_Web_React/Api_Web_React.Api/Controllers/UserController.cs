17d0fb6b40f1c233c5f401080341b0eb7480d824
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2023-07-21 23:30:03 -0500

    Add controller and methods Http

---
+++ Api_Web_React/Api_Web_React.Api/Controllers/UserController.cs
@@ -0,0 +1,101 @@
+﻿using Api_Web_React.Api.Data;
+using Api_Web_React.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Api_Web_React.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public UserController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            try
+            {
+                var users = await _dataContext.Users.FromSqlRaw("EXEC GetUsers").ToListAsync();
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            try
+            {
+                var user = await _dataContext.Users.FirstOrDefaultAsync(x => x.Id == id);
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(User user)
+        {
+            try
+            {
+                _dataContext.Users.Add(user);
+                await _dataContext.SaveChangesAsync();
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest();
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAsync(User user)
+        {
+            try
+            {
+                _dataContext.Users.Update(user);
+                await _dataContext.SaveChangesAsync();
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            try
+            {
+                var user = await _dataContext.Users.FirstOrDefaultAsync(x => x.Id == id);
+                _dataContext.Users.Remove(user);
+                await _dataContext.SaveChangesAsync();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ Api_Web_React/Api_Web_React.Api/Migrations/20230722035219_InitialMigration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api_Web_React.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LasName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Id",
+                table: "Users",
+                column: "Id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
